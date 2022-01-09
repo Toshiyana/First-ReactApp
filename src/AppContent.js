210680e068a3a5306cd3c555186259bf874684b6
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-01-09 15:59:24 +0900

    Update

--- src/AppContent.js
+++ src/AppContent.js
@@ -2,34 +2,52 @@ import React, { Component } from 'react';
 
 export default class AppContent extends Component {
 
+    // state = {posts: []};
+
     constructor(props) {
-        super(props);
-        this.listRef = React.createRef();
-    }
+		super(props);
+		this.handlePostChange = this.handlePostChange.bind(this);
+	}
 
-    anotherFunction = () => {
-        console.log("another function")
-    }
+	handlePostChange(posts) {
+		this.props.handlerPostChange(posts);
+	}
 
-    leftParagraph = () => {
-        console.log("left paragraph")
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.listRef = React.createRef();
+    // }
+
+    // anotherFunction = () => {
+    //     console.log("another function")
+    // }
+
+    // leftParagraph = () => {
+    //     console.log("left paragraph")
+    // }
 
     fetchList = () => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then((response) => response.json())
             .then(json => {
-                console.log(json);
-                // let posts = document.getElementById('post-list');
-                const posts = this.listRef.current;
+                this.handlePostChange(json);
+                // this.setState({posts: json});
 
-                json.forEach(function(obj){
-                    let li = document.createElement('li');
-                    li.appendChild(document.createTextNode(obj.title));
-                    posts.appendChild(li);
-                })
+                // console.log(json);
+                // // let posts = document.getElementById('post-list');
+                // const posts = this.listRef.current;
+
+                // json.forEach(function(obj){
+                //     let li = document.createElement('li');
+                //     li.appendChild(document.createTextNode(obj.title));
+                //     posts.appendChild(li);
+                // })
             })
-        console.log("I was clicked");
+        // console.log("I was clicked");
+    }
+
+    clickedItem = (x) => {
+        console.log("clicked", x)
     }
 
     render() {
@@ -40,13 +58,26 @@ export default class AppContent extends Component {
 
                 <br />
                 <hr />
-                <p onMouseEnter={this.anotherFunction} onMouseLeave={this.leftParagraph}>This is some text</p>
+                {/* <p onMouseEnter={this.anotherFunction} onMouseLeave={this.leftParagraph}>This is some text</p> */}
 
                 <button onClick={this.fetchList} className="btn btn-primary">Fetch Data</button>
 
                 <hr />
 
-                <ul id="post-list" ref={this.listRef}></ul>
+                {/* <ul id="post-list" ref={this.listRef}></ul> */}
+
+                {/* <p>Posts is {this.state.posts.length} items long</p> */}
+                <p>Posts is {this.props.posts.length} items long</p>
+
+                <ul>
+                    {this.props.posts.map((c) => (
+                        <li key={c.id}>
+                            <a href="#!" onClick={() => this.clickedItem(c.id)}>
+                                {c.title}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
             </div>
 
         );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import AppHeader from './AppHeader';
-import AppFooter from './AppFooter';
+// import AppFooter from './AppFooter';
 import AppContent from './AppContent';
+import AppFooterFunctionalComponent from './AppFooterFunctionalComponent';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 
 class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.handlePostChange = this.handlePostChange.bind(this);
+		this.state = {posts: []};
+	}
+
+	handlePostChange(posts) {
+		this.setState({posts: posts});
+	}
 
 	render() {
 		const myProps = {
@@ -19,9 +30,10 @@ class App extends Component {
 
 		return (
 			<div className="app">
-				<AppHeader {...myProps} />
-				<AppContent />
-				<AppFooter />
+				<AppHeader {...myProps} posts={this.state.posts} handlePostChange={this.handlePostChange} />
+				<AppContent handlePostChange={this.handlePostChange} posts={this.state.posts} />
+				{/* <AppFooter /> */}
+				<AppFooterFunctionalComponent myProperty={"Hello, world"}/>
 			</div>
 		);
 	}
